236dc5a073c84187a78331d4591ee7b2bfeca0e8
1901570182 <emanuelleleaes@gmail.com>
2022-06-27 18:05:09 -0300

    corrigindo aula 4

--- src/pages/Subscribe.tsx
+++ src/pages/Subscribe.tsx
@@ -12,6 +12,7 @@ mutation CreateSubscriber($name: String!, $email: String!) {
 `
 export function Subscribe(){
   const navigate = useNavigate()
+
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
 
@@ -19,7 +20,6 @@ export function Subscribe(){
 
   async function handleSubscrible(event: FormEvent){
     event.preventDefault();
-
     await createSubscriber({
       variables:{
         name, 
@@ -28,9 +28,12 @@ export function Subscribe(){
     })
     navigate('/event')
   }
+
   return (
     <div className="min-h-screen bg-blur bg-cover bg-no-repeat flex flex-col items-center">
+      
       <div className="w-full max-w-[1100px] flex items-center justify-between mt-20 mx-auto">
+      
       <div className="max-w-[640px]">
         <Logo />
         <h1 className="mt-8 text-[2.5rem] leading-tight">
@@ -40,6 +43,7 @@ export function Subscribe(){
         Em apenas uma semana você vai dominar na prática uma das tecnologias mais utilizadas e com alta demanda para acessar as melhores oportunidades do mercado.
         </p>
       </div>
+
       <div className="p-8 bg-gray-700 border border-gray-500 rounded">
         <strong className="text-2xl mb-6 block">Inscreva-se gratuitamente</strong> 
         <form onSubmit={handleSubscrible} className="flex flex-col gap-2 w-full">
@@ -47,22 +51,22 @@ export function Subscribe(){
             className="bg-gray-900 rounded px-5 h-14"
             type="text"
             placeholder="Seu nome completo"
-            onChange={event=>setName(event.target.value)}
+            onChange={event => setName(event.target.value)}
           />
-           <input 
+          <input 
             className="bg-gray-900 rounded px-5 h-14"
             type="email"
             placeholder="Digite seu e-mail"
-            onChange={event=>setName(event.target.value)}
+            onChange={event => setEmail(event.target.value)}
           />
-<button
-type="submit"
-disabled={loading}
-className="mt-4 bg-green-500 uppercase py-4 rounded font-bold text-sm hover:bg-green-700 transition-colors disabled:opacity-50"
->
-  Garantir minha vaga
-</button>
-
+         
+          <button
+            type="submit"
+            disabled={loading}
+            className="mt-4 bg-green-500 uppercase py-4 rounded font-bold text-sm hover:bg-green-700 transition-colors disabled:opacity-50"
+          >
+            Garantir minha vaga
+          </button>
 
         </form>
       </div>
